e6360b8bfa3661c68a0f5a20645a515ff0de6895
marjmekk <marjmekk1@gmail.com>
2023-05-07 22:14:09 -0700

    installed node and fetch.js

---
+++ fetch.js
@@ -0,0 +1,8 @@
+async function logJSONData() {
+    const response = await fetch("https://dogapi.dog/api/facts?number=5");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+
+  logJSONData()
+  
